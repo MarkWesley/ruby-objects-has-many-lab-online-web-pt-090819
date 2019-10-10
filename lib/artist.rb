96221726bce6d8e32c6a826f5c67df8318f2dd02
Mark Stone <markwstone85@gmail.com>
2019-10-10 02:17:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist
   attr_accessor :name
-  attr_reader :songs
 
   def initialize(name)
     @name = name
@@ -8,7 +7,40 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    if song.new
+      song.artist 
   end
 
 end
+
+# class Artist
+#   attr_accessor :name
+#
+#   @@song_count = 0
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+#
+#   def add_song(song)
+#     self.songs << song
+#     song.artist = self
+#     @@song_count +=1
+#   end
+#
+#   def add_song_by_name(name)
+#     song = Song.new(name)
+#     self.songs << song
+#     song.artist = self
+#     @@song_count +=1
+#   end
+#
+#   def songs
+#     @songs
+#   end
+#
+#   def self.song_count
+#     @@song_count
+#   end
+# end
