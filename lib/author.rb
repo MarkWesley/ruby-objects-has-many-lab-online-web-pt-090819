7a5fd34e0cf38846862fc639fe8575ea60af32f2
Mark Stone <markwstone85@gmail.com>
2019-10-10 03:39:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,19 @@
 class Author
   attr_accessor :name
 
+  
+
   def initialize(name)
     @name = name
+    @posts = []
   end
 
   def posts
     Post.all.select {|post| post.author = self}
+  end
+
+  def add_post(post)
+    @posts << post
   end
 
 end
